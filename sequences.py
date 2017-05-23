617363e030b7c8ae537c3f6d8005df18f03b0a5d
Nelson Leung <nleung91@gmail.com>
2017-05-23 15:29:12 -0500

    add DRAG rabi

--- sequences.py
+++ sequences.py
@@ -1,14 +1,14 @@
 from sequencer import Sequencer
-from pulse_classes import Gauss, Idle, Ones, Square
+from pulse_classes import Gauss, Idle, Ones, Square, DRAG
+from qutip_experiment import run_qutip_experiment
 
 import numpy as np
 import visdom
 
 
-
 channels = ['charge1', 'flux1', 'charge2', 'flux2',
-                'hetero1_I', 'hetero1_Q', 'hetero2_I', 'hetero2_Q',
-                'm8195a_trig', 'readout1_trig', 'readout2_trig', 'alazar_trig']
+            'hetero1_I', 'hetero1_Q', 'hetero2_I', 'hetero2_Q',
+            'm8195a_trig', 'readout1_trig', 'readout2_trig', 'alazar_trig']
 
 channels_awg = {'charge1': 'm8195a', 'flux1': 'm8195a', 'charge2': 'm8195a', 'flux2': 'm8195a',
                 'hetero1_I': 'tek5014a', 'hetero1_Q': 'tek5014a', 'hetero2_I': 'tek5014a', 'hetero2_Q': 'tek5014a',
@@ -22,7 +22,6 @@ channels_delay = {'readout1_trig': -20, 'readout2_trig': -20, 'alazar_trig': -50
 
 
 def readout(sequencer):
-
     sequencer.sync_channels_time(channels)
 
     sequencer.append('hetero1_I',
@@ -43,26 +42,50 @@ def readout(sequencer):
 
 
 def rabi(sequencer):
-
     # rabi sequences
 
-    for rabi_len in range(0,100,10):
+    for rabi_len in range(0, 10, 1):
         sequencer.new_sequence()
 
         sequencer.append('m8195a_trig', Ones(time=100))
-        sequencer.append('charge1', Gauss(max_amp=0.5, sigma_len=rabi_len, cutoff_sigma=2, freq=4.524, phase=0, plot=False))
+        sequencer.append('charge1',
+                         Gauss(max_amp=0.5, sigma_len=rabi_len, cutoff_sigma=2, freq=3.9, phase=0, plot=False))
         readout(sequencer)
 
         sequencer.end_sequence()
 
-    sequencer.complete(plot=True)
+    return sequencer.complete(plot=True)
+
+
+def drag_rabi(sequencer):
+    # drag_rabi sequences
+
+    freq_ge = 3.9  # GHz
+    alpha = 0.2  # GHz
+
+    freq_lambda = (freq_ge - alpha) / freq_ge
+    optimal_beta = freq_lambda ** 2 / (4 * alpha)
+
+    for rabi_len in range(0, 10, 1):
+        sequencer.new_sequence()
+
+        sequencer.append('m8195a_trig', Ones(time=100))
+        sequencer.append('charge1',
+                         DRAG(A=0.5, beta=optimal_beta, sigma_len=rabi_len, cutoff_sigma=2, freq=3.9, phase=0,
+                              plot=False))
+        readout(sequencer)
+
+        sequencer.end_sequence()
+
+    return sequencer.complete(plot=True)
 
 
 if __name__ == "__main__":
-
     vis = visdom.Visdom()
     vis.close()
 
     sequencer = Sequencer(channels, channels_awg, awg_info, channels_delay)
 
-    multiple_sequences = rabi(sequencer)
+    multiple_sequences = drag_rabi(sequencer)
+
+    run_qutip_experiment(multiple_sequences)
