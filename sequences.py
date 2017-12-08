856a2c5fd4cde8ef0c0f33cee95bc98ad892d504
nelsonleung <nleung91@gmail.com>
2017-12-08 11:47:14 -0600

    allow standalone run

--- sequences.py
+++ sequences.py
@@ -13,7 +13,7 @@ import visdom
 class PulseSequences:
     # channels and awgs
 
-    def set_parameters(self,cfg):
+    def set_parameters(self, cfg):
         self.channels = cfg['channels']
 
         self.channels_awg = cfg['channels_awg']
@@ -25,15 +25,16 @@ class PulseSequences:
 
         # pulse params
         self.drag_pi = {'A': 0.0701200429, 'beta': -0.6998354176626167, 'sigma_len': 3.4692014249759544,
-                   'freq': 4.4995338309483905}
+                        'freq': 4.4995338309483905}
 
         self.qubit_freq = cfg['qubit']['freq']
 
         self.qubit_1_pi = Gauss(max_amp=0.5, sigma_len=7, cutoff_sigma=2, freq=self.qubit_freq, phase=0, plot=False)
 
-        self.multimodes = {'freq': [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9], 'pi_len': [100, 100, 100, 100, 100, 100, 100, 100]}
+        self.multimodes = {'freq': [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9],
+                           'pi_len': [100, 100, 100, 100, 100, 100, 100, 100]}
 
-    def __init__(self,cfg):
+    def __init__(self, cfg):
         self.set_parameters(cfg)
 
 
@@ -78,7 +79,7 @@ class PulseSequences:
                              Square(max_amp=0.5, flat_len=150, ramp_sigma_len=5, cutoff_sigma=2, freq=freq, phase=0,
                                     plot=False))
             sequencer.append('flux1', Idle(time=200))
-            readout_time = readout(sequencer)
+            readout_time = self.readout(sequencer)
             readout_time_list.append(readout_time)
 
             sequencer.end_sequence()
@@ -97,7 +98,7 @@ class PulseSequences:
             sequencer.append('m8195a_trig', Ones(time=100))
             sequencer.append('charge1',
                              Gauss(max_amp=0.5, sigma_len=rabi_len, cutoff_sigma=2, freq=4.5, phase=0, plot=False))
-            readout_time = readout(sequencer)
+            readout_time = self.readout(sequencer)
             readout_time_list.append(readout_time)
 
             sequencer.end_sequence()
@@ -141,7 +142,7 @@ class PulseSequences:
             sequencer.append('charge1',
                              DRAG(A=0.1, beta=optimal_beta, sigma_len=rabi_len, cutoff_sigma=2, freq=4.5, phase=0,
                                   plot=False))
-            readout_time = readout(sequencer)
+            readout_time = self.readout(sequencer)
             readout_time_list.append(readout_time)
 
             sequencer.end_sequence()
@@ -155,7 +156,7 @@ class PulseSequences:
 
         sequencer = Sequencer(self.channels, self.channels_awg, self.awg_info, self.channels_delay)
 
-        multiple_sequences, readout_time_list = eval('self.'+experiment)(sequencer)
+        multiple_sequences, readout_time_list = eval('self.' + experiment)(sequencer)
 
         awg_readout_time_list = self.get_awg_readout_time(readout_time_list)
 
@@ -169,4 +170,23 @@ class PulseSequences:
 
 
 if __name__ == "__main__":
-    run_single_experiment('sideband_rabi')
+    cfg = {
+        "channels": [
+            "charge1", "flux1", "charge2", "flux2",
+            "hetero1_I", "hetero1_Q", "hetero2_I", "hetero2_Q",
+            "m8195a_trig", "readout1_trig", "readout2_trig", "alazar_trig"
+        ],
+        "channels_awg": {"charge1": "m8195a", "flux1": "m8195a", "charge2": "m8195a", "flux2": "m8195a",
+                         "hetero1_I": "tek5014a", "hetero1_Q": "tek5014a", "hetero2_I": "tek5014a",
+                         "hetero2_Q": "tek5014a",
+                         "m8195a_trig": "tek5014a", "readout1_trig": "tek5014a", "readout2_trig": "tek5014a",
+                         "alazar_trig": "tek5014a"},
+        "awg_info": {"m8195a": {"dt": 0.0625, "min_increment": 16, "min_samples": 128, "time_delay": 110},
+                     "tek5014a": {"dt": 0.83333333333, "min_increment": 16, "min_samples": 128, "time_delay": 0}},
+
+        "qubit": {"freq": 4.5}
+    }
+
+    ps = PulseSequences(cfg)
+
+    ps.run_single_experiment('rabi')
