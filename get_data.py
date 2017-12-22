41d37d9ab21139292069ab049df02f0540a86ff0
nelsonleung <nleung91@gmail.com>
2017-12-21 20:36:17 -0600

    update get_iq_data to pick the maximum contrast data

--- get_data.py
+++ get_data.py
@@ -18,12 +18,26 @@ def get_iq_data(expt_data,het_freq = 0.148, td=0, pi_cal = False):
 
     data_cos_list = np.array(data_cos_list)
     data_sin_list = np.array(data_sin_list)
-    data_list = np.sqrt(data_cos_list**2 + data_sin_list**2)
 
     if pi_cal:
-        data_cos_list = (data_cos_list[:-2] - data_cos_list[-2])/(data_cos_list[-1] - data_cos_list[-2])
-        data_sin_list = (data_sin_list[:-2] - data_sin_list[-2])/(data_sin_list[-1] - data_sin_list[-2])
-        data_list = (data_list[:-2] - data_list[-2])/(data_list[-1] - data_list[-2])
 
+        ge_cos = data_cos_list[-1] - data_cos_list[-2]
+        ge_sin = data_sin_list[-1] - data_sin_list[-2]
+
+        ge_mean_vec = np.array([ge_cos,ge_sin])
+
+        data_cos_sin_list = np.array([data_cos_list[:-2] - data_cos_list[-2],data_sin_list[:-2] - data_sin_list[-2]])
+
+        data_list = np.dot(ge_mean_vec,data_cos_sin_list)/np.dot(ge_mean_vec,ge_mean_vec)
+
+
+    else:
+        cos_contrast = np.abs(np.max(data_cos_list)-np.min(data_cos_list))
+        sin_contrast = np.abs(np.max(data_sin_list)-np.min(data_sin_list))
+
+        if cos_contrast > sin_contrast:
+            data_list = data_cos_list
+        else:
+            data_list = data_sin_list
 
     return data_cos_list, data_sin_list, data_list
