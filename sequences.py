c47835e7fe30de7cf9effe9b9a3ddde75d5cfb11
nelsonleung <nleung91@gmail.com>
2017-12-29 18:50:08 -0600

    fix bug in sideband_rabi

--- sequences.py
+++ sequences.py
@@ -110,10 +110,10 @@ class PulseSequences:
             sequencer.new_sequence(self)
 
             for qubit_id in self.expt_cfg['on_qubits']:
-                sequencer.append('charge1', self.qubit_pi[qubit_id])
+                sequencer.append('charge%s'%qubit_id, self.qubit_pi[qubit_id])
                 sequencer.sync_channels_time(self.channels)
                 sequencer.append('flux%s'%qubit_id,
-                                 Square(max_amp=self.expt_cfg['amp'], flat_len=rabi_len, ramp_sigma_len=5, cutoff_sigma=2, freq=rabi_freq, phase=0,
+                                 Square(max_amp=self.expt_cfg['amp'], flat_len=rabi_len, ramp_sigma_len=10, cutoff_sigma=2, freq=rabi_freq, phase=0,
                                         plot=False))
             self.readout(sequencer, self.expt_cfg['on_qubits'])
 
