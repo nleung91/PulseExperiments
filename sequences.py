e8501cfaac2a7d6e9e3478b28f71c2ec0220a8db
nelsonleung <nleung91@gmail.com>
2018-01-11 16:18:16 -0600

    only plot first two

--- sequences.py
+++ sequences.py
@@ -527,34 +527,36 @@ class PulseSequences:
     def photon_transfer_arb(self, sequencer, **kwargs):
         # mm rabi sequences
 
-        for expt_id in range(kwargs['expt_num']):
-            sequencer.new_sequence(self)
+        for repeat_id in range(self.expt_cfg['repeat']):
 
-            sender_id = self.expt_cfg['sender_id']
-            receiver_id = self.expt_cfg['receiver_id']
+            for expt_id in range(kwargs['sequence_num']):
+                sequencer.new_sequence(self)
 
-            sequencer.append('charge%s' % sender_id, self.qubit_pi[sender_id])
-            sequencer.sync_channels_time(['charge%s' % sender_id, 'flux%s' % sender_id, 'flux%s' % receiver_id])
+                sender_id = self.expt_cfg['sender_id']
+                receiver_id = self.expt_cfg['receiver_id']
 
-
-            flux_pulse = self.communication_flux_pi[sender_id]
-            flux_pulse.len = kwargs['send_len'][expt_id]
-            flux_pulse.plot = True
-            if 'send_A_list' in kwargs:
-                flux_pulse.A_list = kwargs['send_A_list'][expt_id]
-            sequencer.append('flux%s'%sender_id,flux_pulse)
-
-            flux_pulse = self.communication_flux_pi[receiver_id]
-            flux_pulse.len = kwargs['rece_len'][expt_id]
-            flux_pulse.plot = True
-            if 'rece_A_list' in kwargs:
-                flux_pulse.A_list = kwargs['rece_A_list'][expt_id]
-            sequencer.append('flux%s'%receiver_id,flux_pulse)
+                sequencer.append('charge%s' % sender_id, self.qubit_pi[sender_id])
+                sequencer.sync_channels_time(['charge%s' % sender_id, 'flux%s' % sender_id, 'flux%s' % receiver_id])
 
 
-            self.readout(sequencer, self.expt_cfg.get('on_qubits',["1","2"]))
+                flux_pulse = self.communication_flux_pi[sender_id]
+                flux_pulse.len = kwargs['send_len'][expt_id]
+                flux_pulse.plot = True if repeat_id == 0 else False
+                if 'send_A_list' in kwargs:
+                    flux_pulse.A_list = kwargs['send_A_list'][expt_id]
+                sequencer.append('flux%s'%sender_id,flux_pulse)
 
-            sequencer.end_sequence()
+                flux_pulse = self.communication_flux_pi[receiver_id]
+                flux_pulse.len = kwargs['rece_len'][expt_id]
+                flux_pulse.plot = True if repeat_id == 0 else False
+                if 'rece_A_list' in kwargs:
+                    flux_pulse.A_list = kwargs['rece_A_list'][expt_id]
+                sequencer.append('flux%s'%receiver_id,flux_pulse)
+
+
+                self.readout(sequencer, self.expt_cfg.get('on_qubits',["1","2"]))
+
+                sequencer.end_sequence()
 
         return sequencer.complete(self, plot=False)
 
