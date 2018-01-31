e6b6b880af755c5ee9f9fa6dd9d176e4143fbb4e
nelsonleung <nleung91@gmail.com>
2018-01-31 08:46:22 -0600

    add ghz creation

--- sequences.py
+++ sequences.py
@@ -908,6 +908,83 @@ class PulseSequences:
 
         return sequencer.complete(self, plot=True)
 
+    def ghz_entanglement_by_half_sideband_tomography(self, sequencer, **kwargs):
+        # rabi sequences
+        sender_id = self.communication['sender_id']
+        receiver_id = self.communication['receiver_id']
+
+        measurement_pulse = [['I','I'], ['I','X'],['I','Y'],['X','I'],['X','X'],['X','Y'],['Y','I'],['Y','X'],['Y','Y'],
+                             ['I','-X'],['I','-Y'],['-X','I'],['-X','-X'],['-X','-Y'],['-Y','I'],['-Y','-X'],['-Y','-Y']]
+
+
+        for expt_id in range(kwargs.get('sequence_num',1)):
+            for qubit_measure in measurement_pulse:
+                sequencer.new_sequence(self)
+
+                sequencer.append('charge%s' % sender_id, self.qubit_pi[sender_id])
+                sequencer.sync_channels_time(['charge%s' % sender_id, 'flux%s' % sender_id, 'flux%s' % receiver_id])
+
+                send_flux_pulse = copy.copy(self.communication_flux_half_transfer[sender_id])
+                if 'send_len' in kwargs:
+                    send_flux_pulse.len = kwargs['send_len'][expt_id]
+                if 'send_A_list' in kwargs:
+                    send_flux_pulse.A_list = kwargs['send_A_list'][expt_id]
+                sequencer.append('flux%s'%sender_id,send_flux_pulse)
+                sequencer.sync_channels_time(['charge%s' % sender_id, 'flux%s' % sender_id])
+                sequencer.append('charge%s' % sender_id, self.qubit_pi[sender_id])
+
+                receiver_flux_pulse = copy.copy(self.communication_flux_half_transfer[receiver_id])
+                if 'rece_len' in kwargs:
+                    receiver_flux_pulse.len = kwargs['rece_len'][expt_id]
+                if 'rece_A_list' in kwargs:
+                    receiver_flux_pulse.A_list = kwargs['rece_A_list'][expt_id]
+                sequencer.append('flux%s'%receiver_id,receiver_flux_pulse)
+
+
+
+                sequencer.sync_channels_time(self.channels)
+
+                qubit_1_measure = qubit_measure[0]
+                qubit_2_measure = qubit_measure[1]
+
+                if qubit_1_measure == 'X':
+                    m_pulse = copy.copy(self.qubit_half_pi['1'])
+                    sequencer.append('charge%s' % '1', m_pulse)
+                elif qubit_1_measure == 'Y':
+                    m_pulse = copy.copy(self.qubit_half_pi['1'])
+                    m_pulse.phase = np.pi/2
+                    sequencer.append('charge%s' % '1', m_pulse)
+                elif qubit_1_measure == '-X':
+                    m_pulse = copy.copy(self.qubit_half_pi['1'])
+                    m_pulse.phase = -np.pi
+                    sequencer.append('charge%s' % '1', m_pulse)
+                elif qubit_1_measure == '-Y':
+                    m_pulse = copy.copy(self.qubit_half_pi['1'])
+                    m_pulse.phase = -np.pi/2
+                    sequencer.append('charge%s' % '1', m_pulse)
+
+                if qubit_2_measure == 'X':
+                    m_pulse = copy.copy(self.qubit_half_pi['2'])
+                    sequencer.append('charge%s' % '2', m_pulse)
+                elif qubit_2_measure == 'Y':
+                    m_pulse = copy.copy(self.qubit_half_pi['2'])
+                    m_pulse.phase = np.pi/2
+                    sequencer.append('charge%s' % '2', m_pulse)
+                elif qubit_2_measure == '-X':
+                    m_pulse = copy.copy(self.qubit_half_pi['2'])
+                    m_pulse.phase = -np.pi
+                    sequencer.append('charge%s' % '2', m_pulse)
+                elif qubit_2_measure == '-Y':
+                    m_pulse = copy.copy(self.qubit_half_pi['2'])
+                    m_pulse.phase = -np.pi/2
+                    sequencer.append('charge%s' % '2', m_pulse)
+
+                self.readout(sequencer)
+
+                sequencer.end_sequence()
+
+        return sequencer.complete(self, plot=True)
+
     def transfer_residue_test(self, sequencer):
         # rabi sequences
         sender_id = self.communication['sender_id']
